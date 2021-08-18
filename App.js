62128c5fa20592a19ded1a4685dcd97349023cff
Carlos Jimenez <lamaravillasan@outlook.com>
2021-08-17 21:46:08 -0700

    Repo in progress.

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hey Twitter!</Text> 
+      <Text> @Vilarjimenez is working something out for all of you! </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
