f610336d11b61e915df2b155991e2fff673ff5de
Carlos Jimenez <lamaravillasan@outlook.com>
2021-08-18 21:41:06 -0700

    Layout and Boxes don't work. Errors while running the app.

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Header from './Components/Header';
+import Boxes from './Components/Boxes';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hey Twitter!</Text> 
-      <Text> @Vilarjimenez is working something out for all of you! </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+  render () {
+    return (
+      <View style={styles.container}> 
+        <Header />
+        <Boxes />
+      </View>
+    )
+  }
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  }
+})
---
+++ Components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default class Header extends React.Component {
+  render () {
+    return (
+      <View style={styles.header}>
+        <Text>Header</Text>
+      </View>
+    )
+  }
+}
+
+
+const styles = StyleSheet.create({
+  header: {
+    width: '100%',
+    height: '15%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#eee',
+  }
+})
