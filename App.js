19c78c281e933d171391f70c83d8af2f0b2876b0
Carlos Jimenez <cjimenez@aer.com>
2022-04-24 14:58:33 -0400

    Cleaning up things.

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import Header from './Components/Header';
-import Boxes from './Components/Boxes';
 
 export default class App extends React.Component {
 
   render () {
     return (
       <View style={styles.container}> 
-        <Header />
-        <Boxes />
+        <p> Hello </p>
       </View>
     )
   }
