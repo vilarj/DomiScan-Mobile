721ca463cd4c3c4f5e1ead9e890cc7c6f9a628fe
Carlos Jimenez <cjimenez@aer.com>
2022-05-02 16:09:50 -0400

    We have a title, input boxes for storing the name of the players, and boxes for the scores.

--- App.js
+++ App.js
@@ -1,31 +1,75 @@
 import React, { useState } from 'react';
-import { Text, TextInput, View, ScrollView } from 'react-native';
+import { Dimensions, StyleSheet, Iamge, Text, TextInput, View, ScrollView } from 'react-native';
 
 const App = () => {
-  const [text, setText] = useState('');
+  const { width } = Dimensions.get("screen")
+
+  const styles = StyleSheet.create({
+    title: {
+      margin: 2,
+      marginTop: 40,
+      paddingVertical: 4,
+      borderWidth: 2,
+      backgroundColor: "#61dafb",
+      textAlign: "center",
+      fontSize: 18,
+      fontWeight: "bold",
+    },
+    playerInput: {
+      borderColor: "black",
+      borderWidth: 0.5,
+      width: width / 2,
+      padding: 5,
+      margin: 2,
+      borderRadius: 20,
+      marginTop: 5,
+    },
+    container: {
+      borderWidth: 0.5,
+      flexDirection: "row",
+      margin: 2,
+      padding: 20,
+    },
+  })
 
   return (
-    <ScrollView>
-      <View style={{padding: 10}}>
-        <TextInput
-          style={{height: 120}}
-          placeholder="Enter the name of player 1"
-          onChangeText={newText => setText(newText)}
-        />
-        <Text style={{padding: 0, fontSize: 42}}></Text>
+    // Main view
+    <View>
+      
+      {/* Title view */}
+      <View>
+        <Text style={[styles.title]}>DomiScan</Text>
+      </View>
+      
+      {/* Players' view - text inputs */}
+      <View style={[styles.playerInput]}>
+        <TextInput placeholder='Player 1'></TextInput>
       </View>
 
-      <View style={{padding: 10}}>
-        <TextInput
-          style={{height: 120}}
-          placeholder="Enter the name of player 2"
-          onChangeText={newText => setText(newText)}
-        />
-        <Text style={{padding: 0, fontSize: 42}}></Text>
+      {/* Score Boxes */}
+      <View style={[styles.container]}>
+        <TextInput placeholder='Score'></TextInput>
+      </View>
+      {/* End of score box */}
+      {/* End of players' view */}
+
+      {/* Players' view - text inputs */}
+      <View style={[styles.playerInput]}>
+        <TextInput placeholder='Player 2'></TextInput>
       </View>
 
-    </ScrollView>
+      {/* Score Boxes */}
+      <View style={[styles.container]}>
+        <TextInput placeholder='Score'></TextInput>
+      </View>
+      {/* End of score box */}
+      {/* End of players' view */}
+
+      
+      {/* End of main view */}
+    </View>
   );
-}
+};
+
 
 export default App;
