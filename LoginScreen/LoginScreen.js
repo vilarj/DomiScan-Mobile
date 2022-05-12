0f6d39a0aadc03128efad19f70f827fccdbf5fdb
Carlos Jimenez <cjimenez@aer.com>
2022-05-12 14:43:57 -0400

    Login Screen concept is done. Need to work on the functionality for subscribing the users.

--- LoginScreen/LoginScreen.js
+++ LoginScreen/LoginScreen.js
@@ -1,15 +1,67 @@
-import { View } from 'react-native'
-import React, { Component } from 'react'
+import { View, ScrollView } from 'react-native'
+import React, { useState } from 'react'
 import IconHeader from './Components/IconHeader'
+import CustomInput from './Components/CustomInput'
+import CustomButton from './Components/CustomButton'
+import Socials from './Components/Socials'
 
-export class LoginScreen extends Component {
-  render() {
-    return (
+const SignInPressed = () => {
+  console.warn('login...')
+}
+
+const ForgotOnPressed = () => {
+  console.warn('Forgot Password...')
+}
+
+const CreateAccount = () => {
+  console.warn('Create Account...')
+}
+
+const LoginScreen = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <ScrollView showsVerticalScrollIndicator='false'>
       <View>
         <IconHeader />
+
+        <CustomInput 
+          placeholder={'username'}
+          value={username}
+          setValue={setUsername}
+        />
+        
+        <CustomInput 
+          placeholder={'Password'}
+          value={password}
+          setValue={setPassword}
+          secureTextEntry={true}
+        />
+
+
+        <CustomButton 
+          text='Sign In'
+          onPress={SignInPressed}
+        />
+
+        <CustomButton
+          text='Forgot Password?'
+          onPress={ForgotOnPressed}
+          type={'tertiary'}
+        />
+
+        <Socials />
+
+        <CustomButton 
+          text="Don't have an account?"
+          onPress={CreateAccount}
+          type={'tertiary'}
+        />
+
       </View>
-    )
-  }
+    </ScrollView>
+  )
 }
 
 export default LoginScreen
