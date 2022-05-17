e6a3ac3bfdf3752ace31a83d54275b350af1ce8c
Carlos Jimenez <cjimenez@aer.com>
2022-05-17 10:41:35 -0400

    Fixing minor issue with the camera component.

--- DomiScan/App.js
+++ DomiScan/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { View } from "react-native";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
+import Camera2 from "./Components/Camera2";
 
 const App = () => {
   return (
     <View>
       <Header />
       <Body />
+      <Camera2 />
     </View>
   );
 };
