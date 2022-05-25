f63e03c05e153492404609a815b3769f318bc14e
Carlos Jimenez <cjimenez@aer.com>
2022-05-25 14:35:58 -0400

    Minor update.

--- DomiScan/App.js
+++ DomiScan/App.js
@@ -1,15 +1,19 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Button } from "react-native";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
-// import Camera2 from "./Components/Camera2";
+import Camera2 from "./Components/Camera2";
+
+const OpenCamera = () => {
+  return <Camera2 />;
+};
 
 const App = () => {
   return (
     <View>
       <Header />
       <Body />
-      {/* <Camera2 /> */}
+      <Button title="Camera" onPress={OpenCamera} />
     </View>
   );
 };
