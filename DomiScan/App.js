f9631a178c2df6d33380e44c6ff4b3f6cc052367
Carlos Jimenez <cjimenez@aer.com>
2022-05-28 20:29:22 -0400

    Minor update

--- DomiScan/App.js
+++ DomiScan/App.js
@@ -4,16 +4,11 @@ import Header from "./Components/Header";
 import Body from "./Components/Body";
 import Camera2 from "./Components/Camera2";
 
-const OpenCamera = () => {
-  return <Camera2 />;
-};
-
 const App = () => {
   return (
     <View>
       <Header />
       <Body />
-      <Button title="Camera" onPress={OpenCamera} />
     </View>
   );
 };
