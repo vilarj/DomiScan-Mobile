03a5bc398d444c7e9e73c64fe6f7d93a33ed22c6
Carlos Jimenez <cjimenez@aer.com>
2022-05-30 15:02:05 -0400

    Prebuild stuff.

--- DomiScan/App.js
+++ DomiScan/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import { View } from "react-native";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
-// import Camera2 from "./Components/Camera2";
 
 const App = () => {
   return (
     <View>
       <Header />
       <Body />
-      {/* <Camera2 /> */}
     </View>
   );
 };
