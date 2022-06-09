654b8818c4419d23c49eebc62862b56df70b9e7d
Carlos Jimenez <cjimenez@advisor360.com>
2022-06-09 19:22:35 -0400

    Backend works properly.

--- DomiScan/App.js
+++ DomiScan/App.js
@@ -1,13 +1,37 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
 
 const App = () => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000")
+      .then(res => res.json())
+      .then(
+        data => {
+          setData(data)
+          console.log(data)
+        }
+      )
+  }, [])
+
   return (
     <View>
       <Header />
       <Body />
+
+      <Text>
+        {
+          (typeof data.Photo === 'undefined')
+            ? (<Text> Loading... </Text>)
+            : (data.Photo.map((Photo, i) =>
+              <Text key={i}>{Photo} </Text>
+            ))
+        }
+
+      </Text>
     </View>
   );
 };
